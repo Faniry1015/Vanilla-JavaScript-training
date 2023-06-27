c1f3c093a1beee9bac625a0fe805f73a3f9178dd
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-06-27 09:51:11 +0300

    Begin Carousel

--- script/main.js
+++ script/main.js
@@ -1,22 +1,47 @@
-//Pour chaque élément observé, lorsqu'elle apparait à l'écran, réalisé l'animation suivante
-
-const observer = new IntersectionObserver((entries) => {
-    for (entry of entries) {
-        if (entry.isIntersecting) {             //isIntersecting: apparait à l'écran
-            entry.target.animate([              //target : l'élément actuel
-                { transform: "translateX(-50px)", opacity: 0 },
-                { transform: "translateX(0)", opacity: 1 }
-            ], {
-                duration: 500
-            })
-            observer.unobserve(entry.target)   //L'animation ne se fait qu'une fois lors du défilement
-        }
+class Carousel {
+    /**
+     * @param {HTMLElement} element 
+     * @param {Object} options 
+     * @param {Object} options.slidesToScroll Nombre d'éléments à faire défiler
+     * @param {Object} options.slidesToScroll Nombre d'éléments visible dans un slide
+     */
+    constructor(element, options = {}) {
+        this.element = element
+        this.options = Object.assign({}, {      //options par défaut si on en entre pas
+            slidesToScroll: 1,                  //Un seul élément scroller
+            slidesVisible: 1                    //Un seul élément visible
+        }, options)
+        this.children = [].slice.call(element.children) 
+        let ratio = this.children.length / this.options.slidesVisible
+        let root = this.createDivWithClass("carousel")
+        let container = this.createDivWithClass("carousel__container")
+        container.style.width = (ratio * 100) +"%"
+        root.appendChild(container)
+        this.element.appendChild(root)
+        this.children.forEach(child => {
+            let item = this.createDivWithClass("carousel__item")
+            item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%"
+            item.appendChild(child)
+            container.appendChild(item)
+        });
     }
-}, {
-    threshold: .2  //20% de l'élément est visible pour considérer qu'il y ai une intersection
-})
-//root margin s'utilise comme threshold pour augmenter les marges à partir desquels l'élément est considéré comme visible
 
-observer.observe(document.querySelector(".btn1"))
-observer.observe(document.querySelector(".btn2"))
-observer.disconnect() //Arrête tout observation (déconnecte tous les observations)
+    setSt
+
+    /**
+    *@param {string} className
+    *@returns {HTMLElement}
+    */ 
+    createDivWithClass(className) {
+        let div = document.createElement("div")
+        div.setAttribute("class", className)
+        return div
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    new Carousel(document.querySelector("#carousel1"), {
+        slidesToScroll: 3,
+        slidesVisible: 3
+    })
+})
