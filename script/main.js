e9bfedac35a24a9e68bd5c04a25258bbfa6f2ca7
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-07-21 10:38:05 +0300

    Révision class JS

--- script/main.js
+++ script/main.js
@@ -1,31 +1,41 @@
-function wait(duration) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(duration)
-        }, duration)
-    })
-}
-
-function waitAndFail(duration) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject(duration)
-        }, duration)
-    })
-}
-
-async function waitAsync(duration, msg) {
-    try {
-        await wait(1000)
-        console.log("1:", msg, "1s")
-        await waitAndFail(duration)
-        console.log(msg, duration)
-        await wait(1000)
-        console.log("2:", msg, "1s")
-    } catch(e) {
-        console.log("Erreur:", e)
+class Rectangle {
+    constructor(length, width) {
+        if (length <=0 || width <=0) {
+            throw new Error("Les côtés d'un rectangle ne peuvent être négatives")
+        }
+        this.length = length
+        this.width = width
     }
 
+    get  perimeter() {
+        return (this.length + this.width)*2
+    } 
+
+    set notes(n) {
+        if(Array.isArray(n)) {  //doit être un tableau
+            this._notes = n
+        } else {
+            throw new Error("Tableau obligatoire")
+        }
+    }
+
+    static color = "blue"  // ne peut s'appeler qu'appartir de la classe
+
+    static fonctionStat() {
+        console.log("Ne s'appel qu'avec \"Rectangle")
+    }
+    
+    size = "big"
 }
 
-waitAsync(2000, "message")
+try {
+    const r1 = new Rectangle(6, 2)
+    console.log(r1)
+    console.log(r1.perimeter) // pas de () avec les get ni set
+    r1.notes = [12, 16] 
+    console.log(r1)
+    console.log(Rectangle.color)
+    console.log(r1.size)
+} catch(e) {
+    console.log(e)
+}
