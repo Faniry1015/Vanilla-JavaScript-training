93ad22dab8cbc72dfbd0c2dcf82c1ae44ca2ad6e
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-07-22 19:09:32 +0300

    JS Fetch

--- script/main.js
+++ script/main.js
@@ -1,58 +1,17 @@
-class Book {
-    constructor(title, pages) {
-        this.title = title
-        this.pages = pages
+async function fetchUsers() {
+    const r = await fetch("https://jsonplaceholder.typicode.com/users", {
+        headers : {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "GET",
+        body: JSON.stringify({title: "Mon premier article"})
+    })
+    if (r.ok === true) {
+        return r.json()
     }
-
-    #page = 1
-    
-    get page() {
-        return this.#page
-    }
-
-    nextPage() {
-        if (this.#page < this.pages) {
-            return this.#page++
-        }
-    }
-
-    closeBook() {
-        return this.#page = 1
-    }
+    throw new Error("Erreur serveur")
 }
 
-class Library {
-    books = []
-    addBook(book) {
-        this.books.push(book)
-    }
-
-    addBooks(b) {
-        b.forEach(element => {
-            this.books.push(element)
-        });
-    }
-
-
-    findBooksByLetter(letter) {
-        return this.books.filter((book) => {
-            return book.title[0].toLowerCase() === letter.toLowerCase()
-        })
-    }
-}
-
-const b = new Book("Le seigneur des anneaux", 200)
-b.nextPage()
-console.log(b.page)
-
-const l = new Library()
-l.addBook(b)
-l.addBooks([
-    new Book("Peter Pan", 100),
-    new Book("Batman", 175),
-    new Book("Super Man", 200),
-    new Book("Salope", 250),
-]
-)
-console.log(l)
-console.log(l.findBooksByLetter("S"))
+fetchUsers()
+    .then(console.log)
