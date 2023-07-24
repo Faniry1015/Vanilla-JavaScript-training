71a9f154f567b0d2a094b9c260f5e5eaf254bf31
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-07-24 07:24:21 +0300

    JS Improve showNewItem() training Todolist

--- TP to do list/script/main.js
+++ TP to do list/script/main.js
@@ -1,48 +1,48 @@
 class CreateList {
     items = []
-    oldItems = []
+    itemId = 0
     addItem(item) {
-        this.items.push(item)
+        this.item = item
     }
 
-    showItems() {
-        this.items.forEach((item) => {
-            const checkInput = document.createElement("input")
-            checkInput.setAttribute("type", "checkbox")
-            checkInput.className = "form-check-input"
-            checkInput.setAttribute("id", `todo-${this.items.indexOf(item) + 1}`)
+    showNewItem() {
+        ++this.itemId
 
-            const checkLabel = document.createElement("label")
-            checkLabel.textContent = item.label
-            checkLabel.setAttribute("for", `todo-${this.items.indexOf(item) + 1}`)
+        const checkInput = document.createElement("input")
+        checkInput.setAttribute("type", "checkbox")
+        checkInput.className = "form-check-input"
+        checkInput.setAttribute("id", `todo-${this.itemId}`)
 
-            const checkLabel2 = document.createElement("label")
-            checkLabel2.className = "ms-auto btn btn-danger btn-sm"
-            checkLabel2.innerHTML = `
+        const checkLabel = document.createElement("label")
+        checkLabel.textContent = this.item.label
+        checkLabel.setAttribute("for", `todo-${this.itemId}`)
+
+        const checkLabel2 = document.createElement("label")
+        checkLabel2.className = "ms-auto btn btn-danger btn-sm"
+        checkLabel2.innerHTML = `
                     <i class="bi-trash">
                     </i>
                 `
 
-            const checkLi = document.createElement("li")
-            checkLi.className = "todo list-group-item d-flex align-items-center"
-            checkLi.append(checkInput, checkLabel, checkLabel2)
-            const listGroup = document.querySelector(".list-group")
-            listGroup.append(checkLi)
-        })
-        this.oldItems.push(this.items)
-        this.items = this.items.splice(0, -1)
+        const checkLi = document.createElement("li")
+        checkLi.className = "todo list-group-item d-flex align-items-center"
+        checkLi.append(checkInput, checkLabel, checkLabel2)
+        const listGroup = document.querySelector(".list-group")
+        listGroup.append(checkLi)
+
+        this.items.push(this.item)
         console.log(this.items)
     }
 
-    removeItem() {
-        const delItemLabel = document.querySelectorAll(".bi-trash")
-        for (let item of delItemLabel) {
-            item.addEventListener("click", (event) => {
-                event.preventDefault()
-                item.parentElement.parentElement.remove()
-            })
-        }
-    }
+    // removeItem() {
+    //     const delItemLabel = document.querySelectorAll(".bi-trash")
+    //     for (let item of delItemLabel) {
+    //         item.addEventListener("click", (event) => {
+    //             event.preventDefault()
+    //             item.parentElement.parentElement.remove()
+    //         })
+    //     }
+    // }
 }
 const mainList = new CreateList()
 
@@ -59,18 +59,18 @@ addBtn.addEventListener("click", (event) => {
     event.preventDefault()
     const newItem = new ToDoListItem(addItemLabel.value)
     mainList.addItem(newItem)
-    mainList.showItems()
+    mainList.showNewItem()
     console.log(mainList)
 })
 
-function ableRemoveItem() {
-    const removeItemLabel = document.querySelectorAll(".bi-trash")
-    for (let item of removeItemLabel) {
-        item.addEventListener("click", (event) => {
-            event.preventDefault()
-            item.parentElement.parentElement.remove()
-        })
-    }
-}
-ableRemoveItem()
+// function ableRemoveItem() {
+//     const removeItemLabel = document.querySelectorAll(".bi-trash")
+//     for (let item of removeItemLabel) {
+//         item.addEventListener("click", (event) => {
+//             event.preventDefault()
+//             item.parentElement.parentElement.remove()
+//         })
+//     }
+// }
+// ableRemoveItem()
 
