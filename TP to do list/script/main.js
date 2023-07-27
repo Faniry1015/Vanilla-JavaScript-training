7d703833a71ac66527c09c850522038e3be7b6ea
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-07-27 04:40:19 +0300

    JS training Todolist filter done solve

--- TP to do list/script/main.js
+++ TP to do list/script/main.js
@@ -52,17 +52,50 @@ class CreateList {
     }
 
     filterDoneTasks() {
-        const doneBtn = document.querySelector("button[data-filter=done]")
-        doneBtn.addEventListener("click", () => {
-            const unchecked = document.querySelectorAll("input[class=form-check-input]:not(:checked)")
-            console.log("uncheck: ", unchecked)
-            // unchecked.forEach((e) => {
-            // console.log(e)
-            // })
-        }) 
+        const unchecked = document.querySelectorAll("input[class=form-check-input]:not(:checked)")
+        const checked = document.querySelectorAll("input[class=form-check-input]:checked")
+        unchecked.forEach((uncheckedBoxe) => {
+            uncheckedBoxe.parentNode.classList.remove("d-flex")
+            uncheckedBoxe.parentNode.hidden = "true"
+            console.log(uncheckedBoxe.parentNode)
+        })
+        checked.forEach((checkedBoxe) => {
+            checkedBoxe.parentNode.classList.add("d-flex")
+            checkedBoxe.parentNode.hidden = "false"
+            console.log(checkedBoxe.parentNode)
+        })
+    }
+
+    filterToDoTasks() {
+        const unchecked = document.querySelectorAll("input[class=form-check-input]:not(:checked)")
+        const checked = document.querySelectorAll("input[class=form-check-input]:checked")
+        unchecked.forEach((uncheckedBoxe) => {
+            uncheckedBoxe.parentNode.classList.add("d-flex")
+            uncheckedBoxe.parentNode.hidden = "true"
+            console.log(uncheckedBoxe.parentNode)
+        })
+        checked.forEach((checkedBoxe) => {
+            checkedBoxe.parentNode.classList.remove("d-flex")
+            checkedBoxe.parentNode.hidden = "false"
+            console.log(checkedBoxe.parentNode)
+        })
+    }
+
+    removeFilterTasks() {
+        const unchecked = document.querySelectorAll("input[class=form-check-input]:not(:checked)")
+        const checked = document.querySelectorAll("input[class=form-check-input]:checked")
+        unchecked.forEach((uncheckedBoxe) => {
+            uncheckedBoxe.parentNode.classList.add("d-flex")
+            uncheckedBoxe.parentNode.hidden = "true"
+            console.log(uncheckedBoxe.parentNode)
+        })
+        checked.forEach((checkedBoxe) => {
+            checkedBoxe.parentNode.classList.remove("d-flex")
+            checkedBoxe.parentNode.hidden = "false"
+            console.log(checkedBoxe.parentNode)
+        })
     }
 }
-const mainList = new CreateList()
 
 class ToDoListItem {
     itemId = null
@@ -71,12 +104,20 @@ class ToDoListItem {
     }
 }
 
+const mainList = new CreateList()
+
 const addItemLabel = document.querySelector(".form-control")
 const addBtn = document.querySelector(".btn-primary")
 addBtn.addEventListener("click", (event) => {
     event.preventDefault()
     const newItem = new ToDoListItem(addItemLabel.value)
     mainList.addItem(newItem)
-    mainList.filterDoneTasks()
     mainList.removeItem()
-})  
+})
+
+const doneBtn = document.querySelector("button[data-filter=done]")
+const toDoBtn = document.querySelector("button[data-filter=done]")
+const removeFilterBtn = document.querySelector("button[data-filter=done]")
+doneBtn.addEventListener("click", () => mainList.filterDoneTasks()) 
+doneBtn.addEventListener("click", () => mainList.filterDoneTasks()) 
+doneBtn.addEventListener("click", () => mainList.filterDoneTasks()) 
