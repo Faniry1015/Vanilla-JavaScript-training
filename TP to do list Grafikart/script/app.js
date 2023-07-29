5bcd3520786d8cb3fc27623fd272c8e07a2ca824
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-07-29 16:31:49 +0300

    JS Training code Todolist Grafikart DONE

---
+++ TP to do list Grafikart/script/app.js
@@ -0,0 +1,19 @@
+import { fetchJSON } from "./functions/api.js";
+import { createElement } from "./functions/dom.js";
+import { TodoList } from "./component/Todolist.js";
+
+try {
+const todos = await fetchJSON("https://jsonplaceholder.typicode.com/todos?_limit=5")
+const list = new TodoList(todos)
+list.appendTo(document.querySelector("#todolist"))
+} catch (e) {
+    const alertElement = createElement("div", {
+        class:"alert alert-danger m-2", 
+        role: "alert"
+    })
+    alertElement.innerText = "Impossible de charger les éléments"
+    document.body.prepend(alertElement)
+    console.error(e)
+}
+
+
