36ecee66cd21ac92600348e9ae8776c698d7e389
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-08-02 07:08:31 +0300

    JS Faniry scrollSpy not perfect

--- ScrollSpy JS/script/main.js
+++ ScrollSpy JS/script/main.js
@@ -1,20 +1,27 @@
-const observer = new IntersectionObserver((sections) => {
+/**
+ * 
+ * @param {string} section 
+ * @param {string} navElement 
+ */
+function observer(section, navElement) {
+    const currentSection = document.getElementById(section)
+    const correspondantNav = document.getElementById(navElement)
 
-for (let section of sections) {
-    if (section.isIntersecting) {
-        console.log(section.isIntersecting)
-        let x = document.getElementById(`liSection${sections.indexOf(section) + 1}`)
-        console.log(sections.indexOf(section) + 1)
-    }
+    const obs = new IntersectionObserver((sections) => {
+        for (let sect of sections) {
+            if (sect.isIntersecting) {
+                correspondantNav.classList.add("observed")
+            } else {
+                correspondantNav.classList.remove("observed")
+            }
+        }
+    }, {
+        threshold: 0.7
+    })
+    obs.observe(currentSection)
 }
-})
 
-const section1 = document.getElementById("section1")
-const section2 = document.getElementById("section2")
-const section3 = document.getElementById("section3")
-const section4 = document.getElementById("section4")
-
-observer.observe(section1)
-observer.observe(section2)
-observer.observe(section3)
-observer.observe(section4)
+observer("section1", "liSection1")
+observer("section2", "liSection2")
+observer("section3", "liSection3")
+observer("section4", "liSection4")
