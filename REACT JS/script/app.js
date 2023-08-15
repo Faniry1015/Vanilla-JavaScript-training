8a18ce98def46584e530e2b2c9391a42fad919c3
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-08-15 10:20:17 +0300

    component JS begin

--- REACT JS/script/app.js
+++ REACT JS/script/app.js
@@ -1,28 +1,25 @@
-let n = 0
+function WelcomeFunc({ name, children }) {
+    return <div>
+        <h1>Bonjour {name}</h1>
+        <p>
+            {children}
+        </p>
+    </div>
 
-const items = ["item 1", "item 2", "item 3"]
-const lis = items.map((item, key) =>  <li key={key}>{item}</li>)
-console.log(lis)
-
-function formatNumber(n) {
-    return n.toString().padStart(2, "0")
 }
 
-function render() {
-    const title = <React.Fragment>
-    <h1>Bonjour 
-    <span> {formatNumber(n) % 2 ? formatNumber(n) : null}</span></h1>
-    <ul>
-        {lis}
-    </ul>
-    </React.Fragment>
-    
-    ReactDOM.render(title, document.querySelector("#app"))
+
+class WelcomeClass extends React.Component {
+
+    render() {
+    return <div>
+        <h1>Bonjour {this.props.name}</h1>
+        <p>
+            {this.props.children}
+        </p>
+    </div>
+    }
 }
 
-setInterval(() => {
-    render()
-    n++
-}, 1000)
 
-
+ReactDOM.render(<WelcomeClass name="Faniry">Faniry est un mec cool</WelcomeClass>, document.querySelector("#app"))
